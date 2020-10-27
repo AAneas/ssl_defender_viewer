9094ef783efff108d32167604ad9fb512bd6d021
AANEAS <alan.aneas@etu.u-bordeaux.fr>
2020-10-27 20:42:59 +0100

    buildGraph V1

--- buildGraph.py
+++ buildGraph.py
@@ -14,9 +14,13 @@ class Graph:
     def __init__(self, board):#, tirs cadrés, ):
         self.board = board
         self.shot_on_target_nodes = []
+        self.opponents_and_shots = {}
         self.onTargetShots()
         self.defender_position_nodes = []
         self.everyGridPosition()
+        self.computeDefending()
+        self.sortByDefendingShots()
+        self.defender_position_nodes_close_to_opponent = self.defender_position_nodes.copy()
         ###self.defending_edges = []
         #self.computeDefending()
         #self.deleteUnnecessaryDefenders()
@@ -26,15 +30,18 @@ class Graph:
     def onTargetShots(self):
         #screen = pygame.display.set_mode(board.size) #numpy.array([1280, 960])
         for opp_id in range(self.board.problem.getNbOpponents()):
+            nb_shots = 0
             kick_dir = 0
             opp_pos = self.board.problem.getOpponent(opp_id)
             while kick_dir < 2 * math.pi:
                 for goal in self.board.problem.goals:
                     goal_pos = goal.kickResult(opp_pos, kick_dir)
                     if goal_pos is not None :
+                        nb_shots = nb_shots + 1
                         s = Shot(opp_pos, kick_dir, goal_pos)
                         self.shot_on_target_nodes.append(s)
                     kick_dir += self.board.problem.theta_step
+            self.opponents_and_shots[str(opp_pos)] = nb_shots
 
     def everyGridPosition(self): # faire fonction plus économique dans laquelle on ne vérifie que les points de la grille dans le range du tir
         #x = 0.0
@@ -42,8 +49,9 @@ class Graph:
         ps = self.board.problem.pos_step
         w = self.board.problem.getFieldWidth()
         h = self.board.problem.getFieldHeight()
-        for x in numpy.arange(0-w, w+ps, ps):
-            for y in numpy.arange(0-h, h+ps, ps):
+        #rr = self.board.problem.robot_radius
+        for x in numpy.arange(0-w, w+ps, ps) :
+            for y in numpy.arange(0-h, h+ps, ps) :
                 d = Defender([x,y])
                 self.defender_position_nodes.append(d)
 
@@ -60,27 +68,12 @@ class Graph:
                     shot.append_defender(defender)
                     #defending_edges.append(Defense(defender, shot))
 
-    """def partition(self, arr, low, high):
-        i = (low-1)
-        pivot = len(arr[high].defending_shots)
-        for j in range(low, high):
-            if len(arr[j].defending_shots) <= pivot:
-                i = i+1
-                arr[i], arr[j] = arr[j], arr[i]
-        arr[i+1], arr[high] = arr[high], arr[i+1]
-        return (i+1)
-    
-    def quickSort(self, arr, low, high):
-        if len(arr) == 1:
-            return arr
-        if low < high:
-            p_i = self.partition(arr, low, high)
-            self.quickSort(arr, low, p_i-1)
-            self.quickSort(arr, p_i+1, high)
-
-    def verifySort(self):
-        for defender in self.defender_position_nodes :
-            print(defender.defending_shots)"""
+    #def verifySort(self):
+        #print("Verify sort :")
+        #for defender in self.defender_position_nodes :
+            #print(len(defender.defending_shots))
+            #if len(defender.defending_shots) == 11 :
+            #    print(defender.pos)
 
     def sortByDefendingShots(self):
         #print("test 1\n")
@@ -95,88 +88,89 @@ class Graph:
     #        if not defender.defending_shots:
     #            defender_position_nodes.remove(defender)
 
-    def verifyShots(self):
-        for s in self.shot_on_target_nodes :
-            print(str(len(s.defenders)))
+    #def verifyShots(self):
+    #    print("Verify shots :")
+    #    for s in self.shot_on_target_nodes :
+    #        print(str(len(s.defenders)))
 
     def deleteUnnecessaryDefenders(self):
-        self.verifyShots()
+        #self.verifyShots()
         # proposition : on commence par regarder les défenseurs qui défendent le moins de buts, pour chacun de ses buts, on regarde s'il est déjà défendu par qqn d'autre, si c'est le cas, on lui enlève cette défense, si une fois qu'on a tout regardé il n'a plus de buts défendus on l'enlève de la liste des noeuds
-        print("Deleting unnecessary defenders\n")
+        #print("Deleting unnecessary defenders\n")
         defender_nb = 0
-        verbose = False
+        #verbose = False
         while defender_nb < len(self.defender_position_nodes) :
             defender = self.defender_position_nodes[defender_nb]
-            if defender.pos == [4.199999999999953, -0.2000000000000206] :
-                verbose = True
-            if verbose :
-                print("defender_nb = "+str(defender_nb))
+            #if defender.pos == [4.199999999999953, -0.2000000000000206] :
+            #    verbose = True
+            #if verbose :
+            #    print("defender_nb = "+str(defender_nb))
         #for defender in self.defender_position_nodes :
             #print("defends "+str(len(defender.defending_shots))+" shots")
             only_defender = False
             for shot in defender.defending_shots :
-                if verbose :
-                    print("len(shot.defenders) = "+str(len(shot.defenders)))
+                #if verbose :
+                #    print("len(shot.defenders) = "+str(len(shot.defenders)))
                 if len(shot.defenders) < 2 :
                     only_defender = True
                     break
-            if verbose :
-                print("only_defender = "+str(only_defender))
+            #if verbose :
+                #print("only_defender = "+str(only_defender))
             if only_defender :
                 defender_nb = defender_nb + 1
-                if verbose :
-                    print("all good")
+                #if verbose :
+                #    print("all good")
             else :
-                if verbose :
-                    print("wait a minute...")
+                #if verbose :
+                #    print("wait a minute...")
                 for shot in defender.defending_shots :
-                    if verbose :
-                        print("shot removed")
+                    #if verbose :
+                    #    print("shot removed")
                     shot.defenders.remove(defender)
                     #defender.defending_shots.remove(shot)
                 self.defender_position_nodes.remove(defender)
-            verbose = False
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            #shot_nb = 0
-            #while shot_nb < len(defender.defending_shots) :
-            #for shot_nb in range(len(defender.defending_shots)) :
-            #for shot in defender.defending_shots :
-                #print("attention shot_nb = "+str(shot_nb)+" et range = "+str(range(len(defender.defending_shots))))
-                #print("before "+str(len(shot.defenders)))
-            #    if len(defender.defending_shots[shot_nb].defenders) > 1 : #il faudra vérifier qu'avec notre implémentation un shot ne peut pas contenir plusieurs fois le même defender
-            #        defender.defending_shots[shot_nb].defenders.remove(defender)
-            #        defender.defending_shots.remove(defender.defending_shots[shot_nb])
-            #    else :
-            #        shot_nb = shot_nb + 1
+            #verbose = False
+        #self.verifyShots()            
 
+    def closeToOpponent(self) :
+        index = len(self.defender_position_nodes_close_to_opponent)-1
+        while index >= 0 :
+            if self.opponents_and_shots is None :
+                break
+            defending_opponent = {}
+            for shot in self.defender_position_nodes_close_to_opponent[index].defending_shots :
+                if str(shot.pos) not in defending_opponent :
+                    defending_opponent[str(shot.pos)] = 1
+                else :
+                    defending_opponent[str(shot.pos)] = defending_opponent[str(shot.pos)] + 1
+            defending = False
+            for opponent in defending_opponent :
+                if (opponent in self.opponents_and_shots) :
+                    if (self.opponents_and_shots[opponent] == defending_opponent[opponent]) :
+                        #print("c'est là")
+                        #print(self.opponents_and_shots[opponent])
+                        #print(defending_opponent[opponent])
+                        #print("c'est fini\n")
+                        self.opponents_and_shots.pop(opponent)
+                        defending = True
+            if defending == False :
+                del self.defender_position_nodes_close_to_opponent[index]
+            index = index - 1
 
-            #    if len(defender.defending_shots[shot_nb].defenders) < 2 : #il faudra vérifier qu'avec notre implémentation un shot ne peut pas contenir plusieurs fois le même defender
-                    #defender.defending_shots[shot_nb].defenders.remove(defender)
-                    #defender.defending_shots.remove(defender.defending_shots[shot_nb])
-            #    else :
-                    #shot_nb = shot_nb + 1
-
-
-                    # si on finit par utiliser defending_edges[] alors il faudra lui remove aussi la denfese qui contient defender et shot, sauf si on ne la crée qu'après avoir supprimé les unnecessary defenders
-                #print("after "+str(len(shot.defenders)))
-            #print("before s "+str(len(defender.defending_shots))+" "+str(len(self.defender_position_nodes)))
-            #print("still defends "+str(len(defender.defending_shots))+" shots")
-            #if len(defender.defending_shots) < 1 :
-            #    self.defender_position_nodes.remove(defender)
-            #else :
-            #    defender_nb = defender_nb + 1
-            #print("after s "+str(len(defender.defending_shots))+" "+str(len(self.defender_position_nodes)))
-        self.verifyShots()            
+    def chooseAnswer(self):
+        #print("choosing")
+        #print(self.areAllShotsDefended())
+        #print(self.opponents_and_shots)
+        #print(len(self.defender_position_nodes_close_to_opponent))
+        #print(len(self.defender_position_nodes))
+        #print("choice : ")
+        if((self.areAllShotsDefended() and (not self.opponents_and_shots)) or ((not self.areAllShotsDefended()) and self.opponents_and_shots)) :
+            if (len(self.defender_position_nodes_close_to_opponent) < len(self.defender_position_nodes)) :
+                self.defender_position_nodes = self.defender_position_nodes_close_to_opponent
+                #print("choice 2")
+        elif ((not self.areAllShotsDefended()) and (not self.opponents_and_shots)) :
+            self.defender_position_nodes = self.defender_position_nodes_close_to_opponent
+            #print("choice 2")
 
     def areAllShotsDefended(self):
         for shot in self.shot_on_target_nodes :
@@ -253,15 +247,17 @@ with open(problem_path) as problem_file:
 
 graph = Graph(Board(problem, None))
 
-graph.computeDefending()
-
-graph.sortByDefendingShots()
-
 graph.deleteUnnecessaryDefenders()
+
+graph.closeToOpponent()
+
+graph.chooseAnswer()
 
 print(graph.isSolution()) #faire qqch de cette info
 
 graph.WriteSolution()
+
+#graph.verifySort()
 
 sys.exit()
 
